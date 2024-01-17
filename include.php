7248aa2f7b46b637904ae920ec07463f045f54d9
ae2019 <31279950+ae2019@users.noreply.github.com>
2024-01-17 16:48:28 +0300

    Add files via upload

---
+++ include.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Файл local/modules/scrollup/include.php
+ */
+
+Bitrix\Main\Loader::registerAutoloadClasses(
+    'scrollup',
+    array(
+        'ScrollUp\\Main' => 'lib/Main.php',
+    )
+);
